7be3691a734400e8e2b5cf4c6263698d06738bcf
EdnaTMunill <80659091+Ethra8@users.noreply.github.com>
2024-01-28 15:17:02 +0100

    beautify code

--- 02-jQueryIntroduction/02-downloading_jquery/script.js
+++ 02-jQueryIntroduction/02-downloading_jquery/script.js
@@ -10,21 +10,21 @@ $(document).ready(function() {
      $(".stream2").removeClass('highlight_stream');
      $(".stream3").removeClass('highlight_stream');
      $(".stream1").addClass('highlight_stream');
-});
-$("#stream1_btn").on("mouseout", function() {
+   });
+   $("#stream1_btn").on("mouseout", function() {
      $(".stream1").removeClass('highlight_stream');
      $(".stream2").removeClass('highlight_stream');
      $(".stream3").removeClass('highlight_stream');
      $(".stream1").removeClass('highlight_stream');
-});
-
+   });
    $("#stream2_btn").on("click", function() {
         $(".stream1").removeClass('highlight_stream');
         $(".stream2").removeClass('highlight_stream');
         $(".stream3").removeClass('highlight_stream');
         $(".stream2").addClass('highlight_stream');
    });
-   $("#stream3_btn").on("click", function() {
+   // same as on("click", function(){}) ===> .click(function(){})
+   $("#stream3_btn").click( function() {
         $(".stream1").removeClass('highlight_stream');
         $(".stream2").removeClass('highlight_stream');
         $(".stream3").removeClass('highlight_stream');
